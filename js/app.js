cdb122c51b33be2f622858fa56117de156a2f244
Naastyya <nastya.slota31@gmail.com>
2023-03-25 17:09:22 +0200

    add new file js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const hostname = '13.42.126.133';
+const port = 3000;
+
+const version = '1.0.0';
+
+app.get('/', (req, res) => {})
+
+app.listen(port, () => {});
