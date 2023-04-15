e7e3b6ecf42e41d3207f7630d70104e5aefadeb3
Roman <romadraguca@gmail.com>
2023-04-15 22:22:24 +0300

    change folder

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+//! Форма регістрації\авторизації
+`use strict`;
+function togglePasswordVisibility(passwordId) {
+  const passwordField = document.getElementById(passwordId);
+  const showPasswordButton = document.getElementById(`show-${passwordId}`);
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    showPasswordButton.innerHTML =
+      '<img class="hide" src="/img/icons8-eye-50.svg">';
+  } else {
+    passwordField.type = "password";
+    showPasswordButton.innerHTML =
+      '<img class="hide" src="/img/icons8-eye-64.svg">';
+  }
+}
+// !Мейн сторінка
 let search = document.getElementById("search");
 let search_icon = document.getElementById("search_icon");
 let subMenu = document.getElementById("subMenu");
