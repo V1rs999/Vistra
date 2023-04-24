7db4b721d4221dfab86d114525a4f67213a19c27
Roman <romadraguca@gmail.com>
2023-04-24 20:29:42 +0300

    Change logic

--- server/Services/PostService.js
+++ server/Services/PostService.js
@@ -4,35 +4,35 @@ const fileService = require("./ImageService.js");
 class PostService {
   async create(post, picture) {
     const fileName = fileService.saveFile(picture);
-    const createdPost = await PostModel.create({ ...post, picture: fileName });
-    return createdPost;
+    return await PostModel.create({ ...post, picture: fileName });
   }
+
   async getAll() {
-    const posts = await PostModel.find();
-    return posts;
+    return PostModel.find();
   }
+
   async getOne(id) {
     if (!id) {
       throw new Error("ID can't be null");
     }
-    const post = await PostModel.findById(id);
-    return post;
+    return PostModel.findById(id);
   }
+
   async update(post) {
     if (!post._id) {
       throw new Error("ID can't be null");
     }
-    const updatePost = await PostModel.findByIdAndUpdate(post._id, post, {
+    return PostModel.findByIdAndUpdate(post._id, post, {
       new: true,
     });
-    return updatePost;
   }
+
   async delete(id) {
     if (!id) {
       throw new Error("ID can't be null");
     }
-    const post = await PostModel.findByIdAndDelete(id);
-    return post;
+    return PostModel.findByIdAndDelete(id);
   }
 }
+
 module.exports = new PostService();
