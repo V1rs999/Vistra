14b45878a937e6e3b650b24b2806465487995a4f
Andrij <andrewpoluhovuch@gmail.com>
2023-04-28 00:01:27 +0300

    Add modal window

--- public/js/index.js
+++ public/js/index.js
@@ -18,6 +18,36 @@ function togglePasswordVisibility(passwordId) {
   }
 }
 
+`use strict`;
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+const escape = document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  } else {
+    console.log(e.key);
+  }
+});
+
+
 
 //
 // const form = document.getElementById('form');
--- public/js/m_f.js
+++ public/js/m_f.js
@@ -662,7 +662,7 @@ const movies = [
   genre7: "documentary",
   genre8: "drama",
   year: 2019,
-  rate: 5.8,
+  rate: 6,
   url: "thevault.html",
   tppr: "popular",
   msot: "movie"
@@ -814,7 +814,8 @@ window.addEventListener('load', () => {
         });
     })
 
-    let letter_a = document.getElementById('letter_a');
+
+let letter_a = document.getElementById('letter_a');
 
 let letter_a_array = movies.filter((e) => {
     return e.letter == "a";
@@ -878,4 +879,126 @@ window.addEventListener('load', () => {
 
             letter_b.appendChild(card);
         });
+    });
+
+
+    let rate8 = document.getElementById('rate8');
+
+    let rate8_array = movies.filter((e) => {
+        return e.rate >= 8;
     })
+    
+    window.addEventListener('load', () => {
+            rate8_array.forEach(element => {
+                const { img, title, year, url, rate } = element;
+                let card = document.createElement('div');
+                card.classList.add('card');
+                card.innerHTML = `
+        <a href="${url}">
+                    <img src="${img}" alt="${title}">
+                    <div class="content">
+                        <h2>${title}</h2>
+                        <div class="metadata">
+                            <span>${year}</span>
+                            <div class="rate">
+                                <i class="fas fa-heart"></i>
+                                <i class="fas fa-eye"></i>
+                                <i class="fas fa-star"></i>
+                                <span>${rate}</span>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+        `
+    
+                rate8.appendChild(card);
+            });
+        });
+
+    let rate4 = document.getElementById('rate4');
+
+    let rate4_array = movies.filter((e) => {
+        return e.rate <= 7;
+    })
+    
+    window.addEventListener('load', () => {
+            rate4_array.forEach(element => {
+                const { img, title, year, url, rate } = element;
+                let card = document.createElement('div');
+                card.classList.add('card');
+                card.innerHTML = `
+        <a href="${url}">
+                    <img src="${img}" alt="${title}">
+                    <div class="content">
+                        <h2>${title}</h2>
+                        <div class="metadata">
+                            <span>${year}</span>
+                            <div class="rate">
+                                <i class="fas fa-heart"></i>
+                                <i class="fas fa-eye"></i>
+                                <i class="fas fa-star"></i>
+                                <span>${rate}</span>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+        `
+    
+                rate4.appendChild(card);
+            });
+        });
+
+        let all_btn = document.getElementById('all_btn');
+
+        all_btn.addEventListener('click', () => {
+            all_btn.classList.toggle('cato_button_active');
+            all.classList.toggle('box2_actives');
+        });
+        
+        let latest_btn = document.getElementById('latest_btn');
+        
+        latest_btn.addEventListener('click', () => {
+            latest_btn.classList.toggle('cato_button_active');
+            latest.classList.toggle('box2_actives');
+        });
+        let year2022__btn = document.getElementById('year2022__btn');
+        
+        year2022__btn.addEventListener('click', () => {
+            year2022__btn.classList.toggle('cato_button_active');
+            year_2022.classList.toggle('box2_actives');
+        });
+        let year2021__btn = document.getElementById('year2021__btn');
+        
+        year2021__btn.addEventListener('click', () => {
+            year2021__btn.classList.toggle('cato_button_active');
+            year_2021.classList.toggle('box2_actives');
+        });
+        
+        let letter_a_btn = document.getElementById('letter_a_btn');
+        
+        letter_a_btn.addEventListener('click', () => {
+            letter_a_btn.classList.toggle('cato_button_active');
+            letter_a.classList.toggle('box2_actives');
+        });
+        let letter_b_btn = document.getElementById('letter_b_btn');
+        
+        letter_b_btn.addEventListener('click', () => {
+            letter_b_btn.classList.toggle('cato_button_active');
+            letter_b.classList.toggle('box2_actives');
+        });
+        // rat change 
+        
+        let change_input = document.getElementById('change_input');
+        
+        change_input.addEventListener('change', () => {
+            if (change_input.value >= 8) {
+                rate8.classList.add('box2_actives');
+            } else {
+                rate8.classList.remove('box2_actives');
+            }
+            if (change_input.value < 8) {
+                rate4.classList.add('box2_actives');
+            } else {
+                rate4.classList.remove('box2_actives');
+            }
+        });
