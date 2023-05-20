1773d395095c6d2778edc7100e8383389f3ad650
Roman <romadraguca@gmail.com>
2023-05-21 00:16:13 +0300

    set up nodemon

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,24 @@
 const { Schema, model } = require("mongoose");
 
 const User = new Schema({
-  username: { type: String, unique: true, required: true },
-  email: { type: String, unique: true },
-  password: { type: String, unique: true },
-  roles: [{ type: String, ref: "Role" }],
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+  },
+  roles: [
+    {
+      type: String,
+      ref: "Role",
+    },
+  ],
 });
 module.exports = model("User", User);
