5b4b5693fae52302554d58f9f53c337e44477b34
Roman <romadraguca@gmail.com>
2023-05-20 20:44:58 +0300

    add cart

--- public/js/trends.js
+++ public/js/trends.js
@@ -1,148 +1,42 @@
-import {movies} from "../js/card_movies.js";
+fetch("http://localhost:5000/api/posts/")
+  .then((response) => response.json())
+  .then((data) => {
+    // Отримано дані з сервера
+    // Додайте карточки на сторінку
 
-let action1 = document.getElementById('action1');
-let action_bx = document.getElementById('action_bx');
+    // Приклад додавання карточок до контейнеру action_bx
+    const action_bx = document.getElementById("action_bx");
+    data.forEach((element) => {
+      const { picture, title, year, url, rate } = element;
+      console.log(url);
+      let card = document.createElement("div");
+      card.classList.add("card");
+      card.innerHTML = `
+        <a href="/player" onclick="setVideoUrl('${url}')">
+          <img src="${picture}" alt="${title}">
+          <div class="content">
+            <h2>${title}</h2>
+            <div class="metadata">
+              <span>${year}</span>
+              <div class="rate">
+                <i class="fas fa-heart"></i>
+                <i class="fas fa-eye"></i>
+                <i class="fas fa-star"></i>
+                <span>${rate}</span>
+              </div>
+            </div>
+          </div>
+        </a>
+      `;
+      action_bx.appendChild(card);
+    });
 
-action1.addEventListener('click', () => {
-    action1.classList.toggle('cato_button_active');
-    action_bx.classList.toggle('movie_box_active');
-});
+    // Додайте карточки до інших контейнерів на аналогічний спосіб
+  })
+  .catch((error) => {
+    console.log("Помилка при отриманні даних з сервера:", error);
+  });
 
-const action_array = movies.filter((e) => {
-    return e.genre == "action";
-});
-
-action_array.forEach(element => {
-    const { img, title, year, url, rate } = element;
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-    `
-
-    action_bx.appendChild(card);
-});
-
-let crime1 = document.getElementById('crime1');
-let crime_bx = document.getElementById('crime_bx');
-
-crime1.addEventListener('click', () => {
-    crime1.classList.toggle('cato_button_active');
-    crime_bx.classList.toggle('movie_box_active');
-});
-
-const crime_array = movies.filter((e) => {
-    return e.genre == "crime";
-});
-
-crime_array.forEach(element => {
-    const { img, title, year, url, rate } = element;
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-    `
-
-    crime_bx.appendChild(card);
-});
-
-let biography1 = document.getElementById('biography1');
-let biography_bx = document.getElementById('biography_bx');
-
-biography1.addEventListener('click', () => {
-    biography1.classList.toggle('cato_button_active');
-    biography_bx.classList.toggle('movie_box_active');
-});
-
-const biography_array = movies.filter((e) => {
-    return e.genre == "biography";
-});
-
-biography_array.forEach(element => {
-    const { img, title, year, url, rate } = element;
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-    `
-
-    biography_bx.appendChild(card);
-});
-
-let drama1 = document.getElementById('drama1');
-let drama_bx = document.getElementById('drama_bx');
-
-drama1.addEventListener('click', () => {
-    drama1.classList.toggle('cato_button_active');
-    drama_bx.classList.toggle('movie_box_active');
-});
-
-const drama_array = movies.filter((e) => {
-    return e.genre == "drama";
-});
-
-drama_array.forEach(element => {
-    const { img, title, year, url, rate } = element;
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div>
-            </a>
-    `
-    drama_bx.appendChild(card);
-});
+function setVideoUrl(url) {
+  console.log(url);
+}
