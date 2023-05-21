d735ee84f1d56e4dbb3ab33f4831d65de0a6ec59
Andrij <andrewpoluhovuch@gmail.com>
2023-05-22 00:16:23 +0300

    update js code

--- public/js/movie_TvShow.js
+++ public/js/movie_TvShow.js
@@ -1,275 +1,240 @@
-import {movies} from "../js/card_movies.js";
+fetch("http://localhost:5000/api/posts/")
+  .then((response) => response.json())
+  .then((data) => {
 
-let all = document.getElementById('all');
+    let all = document.getElementById('all');
+    let all_btn = document.getElementById('all_btn');
 
-window.addEventListener('load', () => {
-    movies.forEach(element => {
-      const { img, title, year, url, rate } = element;
-      let card = document.createElement('div');
-      card.classList.add('card');
-      card.innerHTML = `
-      <a href="${url}">
-                  <img src="${img}" alt="${title}">
-                  <div class="content">
-                      <h2>${title}</h2>
-                      <div class="metadata">
-                          <span>${year}</span>
-                          <div class="rate">
-                              <i class="fas fa-heart"></i>
-                              <i class="fas fa-eye"></i>
-                              <i class="fas fa-star"></i>
-                              <span>${rate}</span>
-                          </div>
-                      </div>
-                  </div>
-              </a>
-        `
-    all.appendChild(card);
-    });
-  })
-// 
-    let year_2022 = document.getElementById('year_2022');
-
-let year2022 = movies.filter((e) => {
-    return e.year == "2022";
-})
-
-window.addEventListener('load', () => {
-        year2022.forEach(element => {
-            const { img, title, year, url, rate } = element;
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div> 
-            </a>
-    `
-
-            year_2022.appendChild(card);
-        });
-    })
-
-
-    // 2021 box start 
-let year_2021 = document.getElementById('year_2021');
-
-let year2021 = movies.filter((e) => {
-    return e.year == "2021";
-})
-
-window.addEventListener('load', () => {
-        year2021.forEach(element => {
-            const { img, title, year, url, rate } = element;
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div> 
-            </a>
-    `
-
-            year_2021.appendChild(card);
-        });
-    })
-
-
-let letter_a = document.getElementById('letter_a');
-
-let letter_a_array = movies.filter((e) => {
-    return e.letter == "a";
-})
-
-window.addEventListener('load', () => {
-        letter_a_array.forEach(element => {
-            const { img, title, year, url, rate } = element;
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div> 
-            </a>
-    `
-
-            letter_a.appendChild(card);
-        });
-    })
-    // letter_b box start 
-let letter_b = document.getElementById('letter_b');
-
-let letter_b_array = movies.filter((e) => {
-    return e.letter == "b";
-})
-
-window.addEventListener('load', () => {
-        letter_b_array.forEach(element => {
-            const { img, title, year, url, rate } = element;
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-    <a href="${url}">
-                <img src="${img}" alt="${title}">
-                <div class="content">
-                    <h2>${title}</h2>
-                    <div class="metadata">
-                        <span>${year}</span>
-                        <div class="rate">
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-eye"></i>
-                            <i class="fas fa-star"></i>
-                            <span>${rate}</span>
-                        </div>
-                    </div>
-                </div> 
-            </a>
-    `
-
-            letter_b.appendChild(card);
-        });
+    all_btn.addEventListener('click', () => {
+        all_btn.classList.toggle('cato_button_active');
+        all.classList.toggle('box2_actives');
     });
 
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const rate8 = document.getElementById('rate8');
-        const rate4 = document.getElementById('rate4');
-      
-        const rate8Array = movies.filter((e) => {
-          return e.rate >= 8;
-        });
-      
-        rate8Array.forEach((element) => {
-          const { img, title, year, url, rate } = element;
-          let card = document.createElement('div');
-          card.classList.add('card');
-          card.innerHTML = `
-            <a href="${url}">
-              <img src="${img}" alt="${title}">
-              <div class="content">
+    const all_array = data.filter((e) => {
+        return e.genre == "action" || e.genre == "drama" || e.genre == "biography" || e.genre == "comady" || e.genre == "documentary" || e.genre == "crime";
+    });
+    all_array.forEach(element => {
+            const { picture, title, year, url, rate } = element;
+            let card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+            <a href="/player?url=${encodeURIComponent(
+                url
+            )}"> <!-- Add the URL as a query parameter -->  
+                <img src="${picture}" alt="${title}">
+                <div class="content">
                 <h2>${title}</h2>
                 <div class="metadata">
-                  <span>${year}</span>
-                  <div class="rate">
+                    <span>${year}</span>
+                    <div class="rate">
                     <i class="fas fa-heart"></i>
                     <i class="fas fa-eye"></i>
                     <i class="fas fa-star"></i>
                     <span>${rate}</span>
-                  </div>
+                    </div>
                 </div>
-              </div>
+                </div>
             </a>
-          `;
-          rate8.appendChild(card);
+            `;
+
+                all.appendChild(card);
+            });
+
+    // ---------------------------------------------------
+
+    
+    const years = [
+        { btnId: 'year2023__btn', containerId: 'year_2023', year: '2023' },
+        { btnId: 'year2022__btn', containerId: 'year_2022', year: '2022' },
+        { btnId: 'year2021__btn', containerId: 'year_2021', year: '2021' },
+        { btnId: 'year2020__btn', containerId: 'year_2020', year: '2020' }
+      ];
+      
+      years.forEach(({ btnId, containerId, year }) => {
+        const btn = document.getElementById(btnId);
+        const container = document.getElementById(containerId);
+      
+        btn.addEventListener('click', () => {
+          btn.classList.toggle('cato_button_active');
+          container.classList.toggle('box2_actives');
         });
       
-        const rate4Array = movies.filter((e) => {
-          return e.rate < 8;
-        });
+        const filteredData = data.filter(e => e.year == year);
       
-        rate4Array.forEach((element) => {
-          const { img, title, year, url, rate } = element;
-          let card = document.createElement('div');
-          card.classList.add('card');
-          card.innerHTML = `
-            <a href="${url}">
-              <img src="${img}" alt="${title}">
-              <div class="content">
-                <h2>${title}</h2>
-                <div class="metadata">
-                  <span>${year}</span>
-                  <div class="rate">
-                    <i class="fas fa-heart"></i>
-                    <i class="fas fa-eye"></i>
-                    <i class="fas fa-star"></i>
-                    <span>${rate}</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-          `;
-          rate4.appendChild(card);
+        filteredData.forEach(element => {
+          const { picture, title, year, url, rate } = element;
+          const card = createCard(picture, title, year, url, rate);
+          container.appendChild(card);
         });
       });
+      
+      function createCard(picture, title, year, url, rate) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <a href="/player?url=${encodeURIComponent(url)}">
+            <img src="${picture}" alt="${title}">
+            <div class="content">
+              <h2>${title}</h2>
+              <div class="metadata">
+                <span>${year}</span>
+                <div class="rate">
+                  <i class="fas fa-heart"></i>
+                  <i class="fas fa-eye"></i>
+                  <i class="fas fa-star"></i>
+                  <span>${rate}</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        `;
+        return card;
+      }
+      
+      
+//   -----------------
 
-        let all_btn = document.getElementById('all_btn');
+const letterFilters = [
+    { btnId: 'letter_a_btn', containerId: 'letter_a', content: 'a' },
+    { btnId: 'letter_b_btn', containerId: 'letter_b', content: 'b' },
+    { btnId: 'letter_c_btn', containerId: 'letter_c', content: 'c' },
+    { btnId: 'letter_d_btn', containerId: 'letter_d', content: 'd' },
+  ];
+  
+  letterFilters.forEach(({ btnId, containerId, content }) => {
+    const button = document.getElementById(btnId);
+    const container = document.getElementById(containerId);
+  
+    button.addEventListener('click', () => {
+      button.classList.toggle('cato_button_active');
+      container.classList.toggle('box2_actives');
+    });
+  
+    const filteredData = data.filter((e) => e.content == content);
+  
+    filteredData.forEach((element) => {
+      const { picture, title, year, url, rate } = element;
+      const card = createCard(picture, title, year, url, rate);
+      container.appendChild(card);
+    });
+  });
+  
+  function createCard(picture, title, year, url, rate) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+      <a href="/player?url=${encodeURIComponent(url)}">
+        <img src="${picture}" alt="${title}">
+        <div class="content">
+          <h2>${title}</h2>
+          <div class="metadata">
+            <span>${year}</span>
+            <div class="rate">
+              <i class="fas fa-heart"></i>
+              <i class="fas fa-eye"></i>
+              <i class="fas fa-star"></i>
+              <span>${rate}</span>
+            </div>
+          </div>
+        </div>
+      </a>
+    `;
+  
+    return card;
+  }
 
-        all_btn.addEventListener('click', () => {
-            all_btn.classList.toggle('cato_button_active');
-            all.classList.toggle('box2_actives');
-        });
-        
-        let year2022__btn = document.getElementById('year2022__btn');
-        
-        year2022__btn.addEventListener('click', () => {
-            year2022__btn.classList.toggle('cato_button_active');
-            year_2022.classList.toggle('box2_actives');
-        });
-        let year2021__btn = document.getElementById('year2021__btn');
-        
-        year2021__btn.addEventListener('click', () => {
-            year2021__btn.classList.toggle('cato_button_active');
-            year_2021.classList.toggle('box2_actives');
-        });
-        
-        let letter_a_btn = document.getElementById('letter_a_btn');
-        
-        letter_a_btn.addEventListener('click', () => {
-            letter_a_btn.classList.toggle('cato_button_active');
-            letter_a.classList.toggle('box2_actives');
-        });
-        let letter_b_btn = document.getElementById('letter_b_btn');
-        
-        letter_b_btn.addEventListener('click', () => {
-            letter_b_btn.classList.toggle('cato_button_active');
-            letter_b.classList.toggle('box2_actives');
-        });
-        // rat change 
-        
-        let change_input = document.getElementById('change_input');
-        
-        change_input.addEventListener('change', () => {
-            if (change_input.value >= 8) {
-                rate8.classList.add('box2_actives');
-            } else {
-                rate8.classList.remove('box2_actives');
-            }
-            if (change_input.value < 8) {
-                rate4.classList.add('box2_actives');
-            } else {
-                rate4.classList.remove('box2_actives');
-            }
-        });
+
+  let change_input = document.getElementById('change_input');
+
+  change_input.addEventListener('change', () => {
+      if (change_input.value >= 5) {
+          rate8.classList.add('box2_actives');
+      } else {
+          rate8.classList.remove('box2_actives');
+      }
+      if (change_input.value <= 5) {
+          rate4.classList.add('box2_actives');
+      } else {
+          rate4.classList.remove('box2_actives');
+      }
+  
+      if (change_input.value == 5){
+          rate4.classList.remove('box2_actives');
+          rate8.classList.remove('box2_actives');
+      }
+  });
+  
+          // -----------------------------
+          let rate8 = document.getElementById('rate8');
+  
+      let rate8_array = data.filter((e) => {
+          return e.rate > 5;
+      })
+  
+          rate8_array.forEach(element => {
+              const { picture, title, year, url, rate } = element;
+              let card = document.createElement("div");
+              card.classList.add("card");
+              card.innerHTML = `
+              <a href="/player?url=${encodeURIComponent(
+                  url
+              )}"> <!-- Add the URL as a query parameter -->  
+                  <img src="${picture}" alt="${title}">
+                  <div class="content">
+                  <h2>${title}</h2>
+                  <div class="metadata">
+                      <span>${year}</span>
+                      <div class="rate">
+                      <i class="fas fa-heart"></i>
+                      <i class="fas fa-eye"></i>
+                      <i class="fas fa-star"></i>
+                      <span>${rate}</span>
+                      </div>
+                  </div>
+                  </div>
+              </a>
+              `;
+  
+              rate8.appendChild(card);
+          });
+      
+      // rate4 box start 
+      let rate4 = document.getElementById('rate4');
+  
+      let rate4_array = data.filter((e) => {
+          return e.rate <= 5;
+      })
+  
+      
+          rate4_array.forEach(element => {
+              const { picture, title, year, url, rate } = element;
+              let card = document.createElement("div");
+              card.classList.add("card");
+              card.innerHTML = `
+              <a href="/player?url=${encodeURIComponent(
+                  url
+              )}"> <!-- Add the URL as a query parameter -->  
+                  <img src="${picture}" alt="${title}">
+                  <div class="content">
+                  <h2>${title}</h2>
+                  <div class="metadata">
+                      <span>${year}</span>
+                      <div class="rate">
+                      <i class="fas fa-heart"></i>
+                      <i class="fas fa-eye"></i>
+                      <i class="fas fa-star"></i>
+                      <span>${rate}</span>
+                      </div>
+                  </div>
+                  </div>
+              </a>
+              `;
+  
+              rate4.appendChild(card);
+          });
+  
+    })
+
+.catch((error) => {
+    console.log("Помилка при отриманні даних з сервера:", error);
+  });
