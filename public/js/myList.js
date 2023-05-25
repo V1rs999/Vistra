d3481a86b708b162314e15d1c625b765ba3efc21
Roman <romadraguca@gmail.com>
2023-05-25 14:30:21 +0300

    FIX Mylist

--- public/js/myList.js
+++ public/js/myList.js
@@ -4,6 +4,8 @@ fetch("http://16.170.236.235:5000/api/posts/")
     // Отримано дані з сервера
     // Додайте карточки на сторінку
 
+
+    // Приклад додавання карточок до контейнеру action_bx
     const genres = [
       { buttonId: "action1", containerId: "action_bx", genre: "action" },
       { buttonId: "drama1", containerId: "drama_bx", genre: "drama" },
@@ -13,18 +15,11 @@ fetch("http://16.170.236.235:5000/api/posts/")
       { buttonId: "crime1", containerId: "crime_bx", genre: "crime" }
     ];
 
-    const createCards = (array, container, myListArray) => {
+    const createCards = (array, container) => {
       array.forEach((element) => {
-        const { id, picture, title, year, url, rate } = element;
+        const { picture, title, year, url, rate } = element;
         const card = document.createElement("div");
         card.classList.add("card");
-
-        // Check if the ID exists in the MyList array
-        const isLiked = myListArray.includes(id);
-
-        // Use the isLiked variable to conditionally add a CSS class or style to the card
-        const cardClass = isLiked ? "liked-card" : "";
-
         card.innerHTML = `
           <a href="/player?url=${encodeURIComponent(url)}">
             <div><img src="${picture}" alt="${title}"></div>
@@ -41,44 +36,21 @@ fetch("http://16.170.236.235:5000/api/posts/")
               </div>
             </div>
           </a>`;
-
-        // Add the cardClass to the card's classList
-        card.classList.add(cardClass);
-
         container.appendChild(card);
       });
     };
 
     const filterByGenre = (genre) => {
-      return data.filter((e) => e.genre === genre);
+      return data.filter((e) => e.genre == genre);
     };
 
     genres.forEach(({ buttonId, containerId, genre }) => {
       const button = document.getElementById(buttonId);
       const container = document.getElementById(containerId);
 
+
       const filteredData = filterByGenre(genre);
-
-      const myListURL = "http://16.170.236.235:5000/auth/MyList";
-
-      fetch(myListURL, {
-        method: "Post",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(filteredData)
-      })
-        .then((response) => response.json())
-        .then((myListData) => {
-          // Extract the array of IDs from myListData
-          const myListArray = myListData.map((item) => item.id);
-
-          // Call createCards with the filtered data and myListArray
-          createCards(filteredData, container, myListArray);
-        })
-        .catch((error) => {
-          console.log("Error updating MyList data:", error);
-        });
+      createCards(filteredData, container);
     });
 
     // Додайте карточки до інших контейнерів на аналогічний спосіб
